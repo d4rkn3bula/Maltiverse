d597be0fb7c015e59fd014986b8ab36ebaefa41e
kris <d4rkn3bula@pm.me>
2020-12-24 18:55:42 -0500

    Initial commit

---
+++ test/maltiverse_search.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''
+Description:  Python script to query IPv4 , Domain, URL and Has functions through the use of Maltiverse's open IoC Search Engine.
+Reference:  https://whatis.maltiverse.com
+            https://github.com/maltiverse/python-maltiverse
+Author:  Kris Rostkowski
+'''
+
+from maltiverse import Maltiverse
+import keyring
+import json
+
+
+def get_ip():
+
+    secret = keyring.get_password("maltiverse", "username")
+    api = Maltiverse(secret)
+    result = api.ip_get("110.189.222.98")
+    #print(json.dumps(result, indent=2, sort_keys=True))
+
+    with open("ip.json", "w") as ip_results:
+        json.dump(result, ip_results)
+
+get_ip()
